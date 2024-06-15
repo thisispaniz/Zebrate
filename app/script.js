3d2ab543d3665d81198099df5a49ceec48acf122
thisispaniz <paniz.ziaie@gmail.com>
2024-06-15 18:57:41 +0200

    sign up page color scheme switched to the current palette.

--- app/script.js
+++ app/script.js
@@ -62,3 +62,38 @@ submitBtn.addEventListener('click', () => {
 });
 
 loadQuestion();
+
+// password stuff
+let passwordInp = document.querySelector('.password-input');
+let passwordChecklist = document.querySelectorAll('.list-item');
+
+
+let validationRegex = [
+    { regex: /.{8,}/ },
+    { regex: /[0-9]/ },
+    { regex: /[a-z]/},
+    { regex: /[A-Z]/},
+    { regex: /[^A-Za-z0-9]/ } 
+];
+
+passwordInp.addEventListener('keyup', () => {
+    validationRegex.forEach((item, i) => {
+        let isValid = item.regex.test(passwordInp.value);
+
+        if (isValid) {
+            passwordChecklist[i].classList.add('checked');
+        } else {
+            passwordChecklist[i].classList.remove('checked');
+        }
+    });
+});
+
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+}
